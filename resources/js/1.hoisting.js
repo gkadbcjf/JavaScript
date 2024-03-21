cb047cbb8fc4d5ca294d33a8ec0d059770ddb1a7
gkadbcjf <gkadbcjf@gmail.com>
2024-03-21 16:08:16 +0900

    java스크립트

---
+++ resources/js/1.hoisting.js
@@ -0,0 +1,25 @@
+/**
+ * Hoisting
+ */
+
+console.log(name1);
+var name1 = "jiwon";
+console.log(name1);
+
+/**
+ * Hoisting은 뭘까?
+ * 
+ * 모든 변수 선언문이 코드의 최상다능로 이동되는 것처럼 느껴지는 현상
+ * let, const 
+ */
+
+console.log(name2);
+let name2 ="유철";
+
+
+
+// const g =function(){
+//     var k = 0;
+// }
+
+// console.log(k);
